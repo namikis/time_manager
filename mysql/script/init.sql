588596d81c3b22c1d448dd6fbb3b3268c68590d8
namikis <koudoldk@icloud.com>
2022-08-06 01:20:56 +0900

    mysql修正

---
+++ mysql/script/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE attendances(
+    id int auto_increment,
+    user_id VARCHAR(60),
+    start_time VARCHAR(40),
+    end_time VARCHAR(40) DEFAULT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME,
+    primary key (id)
+);
+
