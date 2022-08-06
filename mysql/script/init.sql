d994b9dfadd191bbd5deddb2f336f04a3afe521e
namikis <koudoldk@icloud.com>
2022-08-06 12:57:16 +0900

    作業時間カラムを追加

--- mysql/script/init.sql
+++ mysql/script/init.sql
@@ -3,6 +3,7 @@ CREATE TABLE attendances(
     user_id VARCHAR(60),
     start_time VARCHAR(40),
     end_time VARCHAR(40) DEFAULT NULL,
+    working_time VARCHAR(40) DEFAULT NULL,
     created_at DATETIME,
     updated_at DATETIME,
     deleted_at DATETIME,
