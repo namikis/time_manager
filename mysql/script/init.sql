7447b6870b2669ee8b67e02269928bd0cdd03075
namikis <koudoldk@icloud.com>
2022-08-06 14:27:31 +0900

    休憩テーブル追加

--- mysql/script/init.sql
+++ mysql/script/init.sql
@@ -4,9 +4,23 @@ CREATE TABLE attendances(
     start_time VARCHAR(40),
     end_time VARCHAR(40) DEFAULT NULL,
     working_time VARCHAR(40) DEFAULT NULL,
+    breaking_time VARCHAR(40) DEFAULT NULL,
     created_at DATETIME,
     updated_at DATETIME,
     deleted_at DATETIME,
     primary key (id)
+);
+
+CREATE TABLE breaks(
+    id int auto_increment,
+    attendance_id int,
+    start_break_time VARCHAR(40),
+    end_break_time VARCHAR(40) DEFAULT NULL,
+    breaking_time VARCHAR(40) DEFAULT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME,
+    primary key (id),
+    FOREIGN KEY fk_attendance REFERENCES attendances(id)
 );
 
